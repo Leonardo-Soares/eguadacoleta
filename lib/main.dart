f3c1ba2498ad4bc9233e8ad7882d8933c234a4d5
Leonardo Soares <62779334+Leonardo-Soares@users.noreply.github.com>
2022-09-11 11:21:40 -0300

    ✨ NEW FEATURE: Aula 2 - Login Fake

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,116 @@
-
-
+import 'package:flutter/material.dart';
 
 void main() {
-  final isValid = validate(email: "email", password: "password");
-  if (isValid == null) {
-    login(email: "email", password: "password");
-  } 
+  runApp(MaterialApp(
+    theme: ThemeData(primarySwatch: Colors.amber),
+    initialRoute: "/login",
+    routes: {
+      "/login": (context) => LoginPage(),
+      "/home": (context) => HomePage(),
+    },
+  ));
 }
 
-void login({
-  required String email,
-  required String password,
-}) {}
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-String? validate({String? email,  String? password}) {
-  final emailIsValid = validateEmail(email);
-  final passwordIsValid = validatePassword(password);
-
-  if (passwordIsValid == false) {
-    return "A senha está errada";
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Home page"),
+      ),
+    );
   }
-  if (emailIsValid == false) {
-    return "Email está errado";
-  }
-
-  return "Sucesso";
 }
 
-bool validateEmail(String? email) => email != null && email.length > 0;
-bool validatePassword(String? password) => password != null && password.length > 8;
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
 
-void validateLogin({required String email, required String password}) {
-  print("Conectando");
-  print("Login sucesso");
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
 
+class _LoginPageState extends State<LoginPage> {
+  String? email = "";
+  String? password = "";
+  final formKey = GlobalKey<FormState>();
+  var isLoading = false;
+  var isLogged = false;
+
+  void login({
+    required String email,
+    required String password,
+  }) async {
+    isLoading = true;
+    setState(() {});
+    final response = await apiLogin(email: email, password: password);
+    isLoading = false;
+    setState(() {});
+    if (response) {
+      Navigator.pushNamed(context, "/home");
+    }
+  }
+
+  bool validate() {
+    final form = formKey.currentState!;
+    if (form.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
+  }
+
+  String? validateEmail(String? email) =>
+      email != null && email.isNotEmpty ? null : "Email incorreto";
+  String? validatePassword(String? password) =>
+      password != null && password.length > 8 ? null : "Senha incorreta";
+
+  Future<bool> apiLogin(
+      {required String email, required String password}) async {
+    await Future.delayed(const Duration(seconds: 3));
+    return true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return isLogged
+        ? HomePage()
+        : Scaffold(
+            body: Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: "Email"),
+                      validator: (value) => validateEmail(value),
+                      onSaved: (value) => email = value,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(labelText: "Senha"),
+                      validator: (value) => validatePassword(value),
+                      onSaved: (value) => password = value,
+                    ),
+                    if (isLoading)
+                      const CircularProgressIndicator()
+                    else
+                      TextButton(
+                        onPressed: () {
+                          if (validate()) {
+                            login(email: email!, password: password!);
+                            print("Email: $email | Senha: $password");
+                          }
+                        },
+                        child: const Text("Entrar"),
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          );
+  }
 }
